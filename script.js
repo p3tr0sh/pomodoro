4518c9d01e0b5afdd6560f3957fd27b6f55e2856
petrosh <petrosh@petrosh.de>
2020-03-20 16:05:31 +0100

    fixing count bug, introducing reset phase button and phase object

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
-const workTime = 25*60;
-const shortBreakTime = 5*60;
-const longBreakTime = 20*60;
-const cycles = 6;
-var currentCycle = 0;
-var countWorkTime = 0;
+
+var Phase = {
+	titles: ['Work Phase', 'Short Break', 'LongBreak', 'Break', 'Pause'],
+	times: [25*60, 5*60, 20*60],
+	WORK: 0,
+	SHORT: 1,
+	LONG: 2,
+	BREAK: 3,
+	index: 0,
+	workCycles: 1,
+	getTitle: function() {
+		if(this.index == this.WORK) {
+			return this.titles[this.index] + " " + this.workCycles;
+		}
+		return this.titles[this.index];
+	},
+	getTime: function() {return this.times[this.index];}
+}
+
+const cycles = 4;
 var paused = true;
-var seconds = workTime;
-var phaseText = "";
-var phases = ['Work Phase', 'Short Break', 'LongBreak', 'Break', 'Pause'];
-var phaseIndex = 0;
+var seconds = Phase.getTime();
 
 // keyboard listener space bar
 
@@ -18,36 +29,32 @@ function pad(seconds){
 	return ("00" + minutes).slice(-2)+":"+("00" + seconds).slice(-2);
 }
 
-// Astrid mode, currently broken
+// Astrid mode
 function automatic() {
 	return setInterval(function() {
 		if(seconds == 0){
-			if(currentCycle == cycles) {
-				seconds = longBreakTime;
-				phaseIndex = 2;
-				phaseText = phases[phaseIndex];
-				currentCycle = 0;
-			} else if(currentCycle%2 == 0) {
-				seconds = shortBreakTime;
-				phaseIndex = 1;
-				phaseText = phases[phaseIndex]
-				currentCycle++;
-			} else if(currentCycle%2 == 1){
-				seconds = workTime;	
-				countWorkTime++;
-				phaseIndex = 0;
-				phaseText = phases[phaseIndex] + ' ' + countWorkTime;
-				currentCycle++;
+			if(Phase.index != Phase.WORK){ // work
+				Phase.index = 0;
+				log('started ' + Phase.getTitle());
+			} else {
+				if(Phase.index == Phase.WORK && Phase.workCycles % cycles == 0) { // long break
+					Phase.index = 2;
+					Phase.workCycles++; // increment work counter once at end of work phase
+				} else if(Phase.index == Phase.WORK) { // short break
+					Phase.index = 1;
+					Phase.workCycles++; // increment work counter once at end of work phase
+				}
+				log('started ' + Phase.getTitle());
 			}
-			log('started ' + phaseText);
-			notifyMe(phaseText);
+			seconds = Phase.getTime();
+			notifyMe(Phase.getTitle());
 			
 		} else{
 			if(!paused) {
 				seconds--;
-				document.getElementById('timer_display').innerHTML = pad(seconds);
 			}
 		}
+		document.getElementById('timer_display').innerHTML = pad(seconds);
 	}, 1000);
 }
 
@@ -57,11 +64,11 @@ function manual() {
 		if(paused)
 			return;
 		if(seconds == 0) {
-			seconds = workTime;
-			phaseIndex = 3;
-			phaseText = phases[phaseIndex];
+			seconds = Phase.times[Phase.WORK];
+			Phase.index = 3;
 			pause('started');
-			notifyMe(phaseText);
+			notifyMe(Phase.getTitle());
+			Phase.workCycles++; // increment work counter once at end of work phase
 		} else{
 			seconds--;
 		}
@@ -87,11 +94,9 @@ function log(customText) {
 
 function pause(optionalText) {
 	paused = !paused;
-	if(seconds == workTime && !paused) {
-		// starting new work phase from manual mode?
-		countWorkTime++;
-		phaseIndex = 0;
-		phaseText = phases[phaseIndex] + ' ' + countWorkTime;
+	if(Phase.index == Phase.BREAK && !paused) {
+		// starting new work phase from manual mode
+		Phase.index = 0;
 	}
 	var prefix = ""
 	if(paused) {
@@ -100,13 +105,25 @@ function pause(optionalText) {
 	} else {
 		document.getElementById('pause_button').innerHTML = "Pause";
 		prefix = "started";
-		if((phaseIndex == 0 && seconds < workTime) || (phaseIndex == 1 && seconds < shortBreakTime) || (phaseIndex == 2 && seconds < longBreakTime)) {
+		if((Phase.index == Phase.WORK && seconds < Phase.times[Phase.WORK]) || 
+			(Phase.index == Phase.SHORT && seconds < Phase.times[Phase.SHORT]) || 
+			(Phase.index == Phase.LONG && seconds < Phase.times[Phase.LONG])) {
 			prefix = 'resumed';
 		}
 	}
 	if(typeof optionalText !== "undefined")
 		prefix = optionalText;
-	log(prefix + ' ' + phaseText);
+	if(Phase.index == Phase.WORK) {
+		log(prefix + ' ' + Phase.getTitle());
+	} else {
+		log(prefix + ' ' + Phase.getTitle());
+	}
+}
+
+function reset_phase() {
+	seconds = Phase.getTime();
+	log('reset ' + Phase.getTitle());
+	document.getElementById('timer_display').innerHTML = pad(seconds);
 }
 
 function setMode(val) {
@@ -121,7 +138,7 @@ function setMode(val) {
 
 
 document.addEventListener('DOMContentLoaded', () => {
-	document.getElementById('timer_display').innerHTML = pad(workTime)
+	document.getElementById('timer_display').innerHTML = pad(Phase.getTime())
 	
 	document.getElementById('mode_switcher').onchange = function() {
 		// change mode
